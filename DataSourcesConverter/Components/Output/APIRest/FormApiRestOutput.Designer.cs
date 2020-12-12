92f8191c232a8f64b716bfb5fa318d4e2d74c651
Fábio da Silva Baptista <2181832@my.ipleiria.pt>
2020-12-12 17:00:44 +0000

    ApiRestOutput design

--- DataSourcesConverter/Components/Output/APIRest/FormApiRestOutput.Designer.cs
+++ DataSourcesConverter/Components/Output/APIRest/FormApiRestOutput.Designer.cs
@@ -32,26 +32,25 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.cbMethod = new System.Windows.Forms.ComboBox();
-            this.bt_cancel = new System.Windows.Forms.Button();
-            this.bt_save = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.tb_url = new System.Windows.Forms.TextBox();
+            this.btCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tbName
             // 
             this.tbName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbName.Location = new System.Drawing.Point(9, 62);
+            this.tbName.Location = new System.Drawing.Point(70, 20);
             this.tbName.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(734, 26);
+            this.tbName.Size = new System.Drawing.Size(373, 26);
             this.tbName.TabIndex = 12;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 23);
+            this.label4.Location = new System.Drawing.Point(5, 23);
             this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(55, 20);
@@ -61,7 +60,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 203);
+            this.label3.Location = new System.Drawing.Point(5, 108);
             this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 20);
@@ -78,67 +77,53 @@
             "PUT",
             "DELETE",
             "PATCH"});
-            this.cbMethod.Location = new System.Drawing.Point(9, 241);
+            this.cbMethod.Location = new System.Drawing.Point(84, 108);
             this.cbMethod.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.cbMethod.Name = "cbMethod";
             this.cbMethod.Size = new System.Drawing.Size(334, 28);
             this.cbMethod.TabIndex = 16;
             // 
-            // bt_cancel
-            // 
-            this.bt_cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.bt_cancel.Location = new System.Drawing.Point(383, 282);
-            this.bt_cancel.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
-            this.bt_cancel.Name = "bt_cancel";
-            this.bt_cancel.Size = new System.Drawing.Size(168, 55);
-            this.bt_cancel.TabIndex = 19;
-            this.bt_cancel.Text = "Cancelar";
-            this.bt_cancel.UseVisualStyleBackColor = true;
-            this.bt_cancel.Click += new System.EventHandler(this.bt_cancel_Click);
-            // 
-            // bt_save
-            // 
-            this.bt_save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.bt_save.Location = new System.Drawing.Point(571, 282);
-            this.bt_save.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
-            this.bt_save.Name = "bt_save";
-            this.bt_save.Size = new System.Drawing.Size(168, 55);
-            this.bt_save.TabIndex = 20;
-            this.bt_save.Text = "Guardar";
-            this.bt_save.UseVisualStyleBackColor = true;
-            this.bt_save.Click += new System.EventHandler(this.bt_save_Click);
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 109);
+            this.label1.Location = new System.Drawing.Point(25, 67);
             this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.Size = new System.Drawing.Size(33, 20);
             this.label1.TabIndex = 14;
-            this.label1.Text = "Link:";
+            this.label1.Text = "Url:";
             // 
             // tb_url
             // 
             this.tb_url.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tb_url.Location = new System.Drawing.Point(9, 147);
+            this.tb_url.Location = new System.Drawing.Point(70, 64);
             this.tb_url.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.tb_url.Name = "tb_url";
-            this.tb_url.Size = new System.Drawing.Size(527, 26);
+            this.tb_url.Size = new System.Drawing.Size(373, 26);
             this.tb_url.TabIndex = 13;
+            // 
+            // btCancel
+            // 
+            this.btCancel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btCancel.Location = new System.Drawing.Point(108, 181);
+            this.btCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(112, 35);
+            this.btCancel.TabIndex = 23;
+            this.btCancel.Text = "Cancelar";
+            this.btCancel.UseVisualStyleBackColor = true;
             // 
             // FormApiRestOutput
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(754, 354);
+            this.ClientSize = new System.Drawing.Size(458, 374);
+            this.Controls.Add(this.btCancel);
             this.Controls.Add(this.tbName);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.cbMethod);
-            this.Controls.Add(this.bt_cancel);
-            this.Controls.Add(this.bt_save);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tb_url);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -156,9 +141,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox cbMethod;
-        private System.Windows.Forms.Button bt_cancel;
-        private System.Windows.Forms.Button bt_save;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tb_url;
+        private System.Windows.Forms.Button btCancel;
     }
 }

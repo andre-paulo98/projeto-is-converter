37670ea2ccc674cb7ff013cd1e94a581cb02bafc
André Paulo <andre_duivenvoorden@hotmail.com>
2020-12-13 12:16:46 +0000

    Fixed Logger problem @andrejrcarvalho

---
+++ DataSourcesConverter/Utils/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSourcesConverter {
+
+    public delegate void LoggerCallback(string level, string line);
+    public sealed class Logger {
+        private static Logger instance = null;
+        private static readonly object padlock = new object();
+
+        public LoggerCallback callback { get; set; }
+
+
+        Logger() {
+        }
+
+        public static Logger Instance {
+            get {
+                lock (padlock) {
+                    if (instance == null) {
+                        instance = new Logger();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void error(string from, string msg) {
+            callback("ERROR", $"{from}: {msg}\n");
+        }
+        public void success(string from, string msg) {
+            callback("SUCCESS", $"{from}: {msg}\n");
+        }
+        public void status(string from, string msg) {
+            callback("STATUS", $"{from}: {msg}\n");
+        }
+        public void info(string from, string msg) {
+            callback("INFO", $"{from}: {msg}\n");
+        }
+    }
+}
